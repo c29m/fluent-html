849cb050b324f4dc4ebdf41129ae058826e277ca
c29m <c29.hunter@gmail.com>
2011-02-15 23:02:05 -0200

    Added H4 and HR tags

---
+++ src/FluentHtml/Hr.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FluentHtml
+{
+    public class Hr : Control
+    {
+        public Hr()
+            : base("hr")
+        {
+        }        
+    }
+}
