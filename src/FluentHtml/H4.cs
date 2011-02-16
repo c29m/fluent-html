2963d0fd93bf73fd3a778c756c2d2a2388cf6da1
tucaz <me@tucaz.net>
2011-02-15 23:02:05 -0200

    Added H4 and HR tags

---
+++ src/FluentHtml/H4.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FluentHtml
+{
+    public class H4 : Control
+    {
+        public H4()
+            : base("h4")
+        {
+        }
+
+        public H4(string cssClass, string headerText)
+            : this()
+        {
+            InnerText(headerText);
+            Class(cssClass);
+        }
+    }
+}
